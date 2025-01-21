2878dfcbfc84e33edbd7829041de83e10b03bdd3
LaurelineD <laurelineduf@proton.me>
2025-01-21 16:21:49 +0100

    Delete recherche.c

--- recherche.c
+++
@@ -1,6 +0,0 @@
-https://www.redhat.com/fr/topics/virtualization/what-is-a-virtual-machine => explique bien
-https://tutobox.fr/tuto-debian-mot-de-passe-root-perdu-comment-faire
-https://www.linuxtricks.fr/wiki/sudo-utiliser-et-parametrer-sudoers
-https://cloud.theodo.com/blog/partitionnement-disques-lvm
-https://stackoverflow.com/questions/47806576/username-is-not-in-the-sudoers-file-this-incident-will-be-reported
-https://www.debian.org/doc/manuals/aptitude/ch02s01s04.fr.html
